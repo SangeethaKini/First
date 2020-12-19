1b0e5edcae7ed9084797c473ac12986acc8db060
SangeethaKini <74597400+SangeethaKini@users.noreply.github.com>
2020-12-19 18:22:26 +0530

    Add files via upload

---
+++ Intro.py
@@ -0,0 +1,28 @@
+# # Print Welcome Message
+# message = 'Hello World'
+
+# print(len(message))
+# print(message[6:])  #First index inclusive ; second is exclusive
+
+
+
+# print(message.upper()) #Lower and upper
+
+# print(message.count('l'))
+
+# print(message.find('World'))
+
+# message = message.replace('World','Universe')
+# print(message)
+
+# greeting = 'Hello'
+name = 'Michael'
+
+# #message = greeting + ', ' + name + ' Welcome'
+
+# #message ='{}, {}. Welcome!'.format(greeting,name)
+# message =f'{greeting.upper()}, {name}. Welcome!'
+
+# print(message)
+
+print(help(str.lower))
